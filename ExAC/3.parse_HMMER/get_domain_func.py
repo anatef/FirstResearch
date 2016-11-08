2aa363379feb013bb7ec338c4e54df8e7680a79c
anatef <anatf@princeton.edu>
2016-11-08 13:38:31 -0500

    Parsing new HMMER results format from Shilpa
    Changed the parsing function to parse more fields

--- ExAC/3.parse_HMMER/get_domain_func.py
+++ ExAC/3.parse_HMMER/get_domain_func.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import unicodedata
-
+from collections import defaultdict
 
 def process_hmmer_results(allhmm):
     """
@@ -18,35 +18,40 @@ def process_hmmer_results(allhmm):
     cols = cols[0:1] + cols[-2:] + cols[1:-2]
     allhmm = allhmm[cols]
 
-    #Splitting the Description
-    allhmm['prot_id'], allhmm['ensembl_id'], allhmm['transcript_id'], allhmm['chromosome_id'], allhmm['length'], allhmm['refseq_hmm_start_end'] = zip(*allhmm['Description'].apply(lambda x: x.split(' ', 5)))
-    del allhmm['Description']
+    #Parsing the description field
+    description_headers = ["prot", "pep", "chromosome", "gene", "transcript", "gene_biotype", "transcript_biotype", "hgncID",
+                           "hugoSymbol", "refseq", "entrez", "length", "HMMStart", "HMMEnd"]
+    descriptions_parsed = defaultdict(list)
+    desc_list = allhmm["Description"].tolist()
+    
+    #Iterating over the Description field and parsing the data
+    for i in range(len(desc_list)):
+        curr_desc = desc_list[i].split(' ')
+        desc_idx = 0
+        #Iterating over the headers list and looking for the headers in the parsed data
+        for headers_idx in range(len(description_headers)):
+            val = curr_desc[desc_idx]
+            if (val.find(description_headers[headers_idx]) == 0):
+                if (description_headers[headers_idx] in ("HMMStart", "HMMEnd")):
+                     parsed = val[val.find("=")+1:-1] 
+                else:
+                    parsed = val[val.find(":")+1:]
+                descriptions_parsed[description_headers[headers_idx]].append(parsed)
+                desc_idx += 1
+            else:
+                #If the headers isn't found, appending empty string instead
+                descriptions_parsed[description_headers[headers_idx]].append("")
 
-    #Splitting the last column to deal with missing refseq ids
-    allhmm['refseq'] = allhmm['refseq_hmm_start_end'].apply(lambda x: x[x.find("refseq:")+7:x.find("HMMStart")])
-    allhmm["hmm_start"] = allhmm['refseq_hmm_start_end'].apply(lambda x: x[x.find("HMMStart")+9:x.find(";")])
-    allhmm["hmm_end"] = allhmm['refseq_hmm_start_end'].apply(lambda x: x[x.find("HMMEnd")+7:-1])
-    del allhmm['refseq_hmm_start_end']
-
-    #Extracting the numbers alone from the description columns
-    allhmm['prot_id'] = allhmm['prot_id'].apply(lambda x: x[x.find(':')+1:])
-    allhmm['ensembl_id'] = allhmm['ensembl_id'].apply(lambda x: x[x.find(':')+1:])
-    allhmm['transcript_id'] = allhmm['transcript_id'].apply(lambda x: x[x.find(':')+1:])
-    allhmm['chromosome_id'] = allhmm['chromosome_id'].apply(lambda x: x[x.find(':')+1:])
-    allhmm['length'] = allhmm['length'].apply(lambda x: x[x.find(':')+1:])
-
-    #Extract only the hugo symbol (without the .number) and add to a new column
-    allhmm["Hugo_symbol"] = allhmm['#TargetID'].apply(lambda x: x.split('.')[0])
-    #Get the columns to the original order
-    cols = allhmm.columns.tolist()
-    cols = cols[0:1] + cols[-1:] + cols[1:-1]
-    allhmm = allhmm[cols]
-
+    #Adding the parsed description to the data frame
+    del allhmm["Description"]
+    for col_name in description_headers:
+        allhmm[col_name] = descriptions_parsed[col_name]
+    
     #Seperate chromosome number to a different column
-    allhmm["chrom_num"] = allhmm["chromosome_id"].apply(lambda x: x[x.find(":")+1:x.find(":", x.find(":")+1)])
+    allhmm["chrom_num"] = allhmm["chromosome"].apply(lambda x: x[x.find(":")+1:x.find(":", x.find(":")+1)])
     #Get the columns to the original order
     cols = allhmm.columns.tolist()
-    cols = cols[:15] + cols[-1:] + cols[15:-1]
+    cols = cols[0:13]+cols[-1:]+cols[13:-1]
     allhmm = allhmm[cols]
     
     return allhmm
