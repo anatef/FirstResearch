97f77570423b5c71f86c3de2483dd4b3313affc3
Anat Etzion-Fuchs <anatf@princeton.edu>
2018-07-20 16:36:10 -0400

    Adding class_weight option to different models.
    Adding different options for weighting the penalizations on the classes.

--- utils/generate_hyperparameter_trials.py
+++ utils/generate_hyperparameter_trials.py
@@ -1,8 +1,10 @@
 import numpy as np
+import random
 
 def generate_trials_NN(no_trials, learning_rate_ub, learning_rate_lb, batch_size_ub, 
                        batch_size_lb,weight_decay_ub, weight_decay_lb, beta_ub, beta_lb, 
                        hidden_units_1_ub, hidden_units_1_lb, hidden_units_2_ub, hidden_units_2_lb):
+    
     np.random.seed(0)
     hyperparameter_trials = []
     for trial in range(no_trials):
@@ -32,7 +34,9 @@ def generate_trials_XGB(no_trials, n_estimators_ub, n_estimators_lb,
                        max_depth_ub, max_depth_lb, min_child_weight_ub,
                        min_child_weight_lb, colsample_bytree_ub, colsample_bytree_lb,
                        gamma_ub, gamma_lb, learning_rate_lb, learning_rate_ub):
+    
     np.random.seed(0)
+    random.seed(0)
     hyperparameter_trials = []
     for trial in range(no_trials):
 
@@ -42,7 +46,8 @@ def generate_trials_XGB(no_trials, n_estimators_ub, n_estimators_lb,
                 "min_child_weight": np.random.uniform(min_child_weight_lb,min_child_weight_ub), 
                 "colsample_bytree": np.random.uniform(colsample_bytree_lb,colsample_bytree_ub), 
                 "gamma": 10 ** (np.random.uniform(gamma_lb, gamma_ub)),
-                "learning_rate": 10 ** (np.random.uniform(learning_rate_lb,learning_rate_ub))
+                "learning_rate": 10 ** (np.random.uniform(learning_rate_lb,learning_rate_ub)),
+                "scale_pos_weight": random.choice([1, "balanced", 0.1])
                }
         hyperparameter_trials.append(trial_dict)
 
@@ -53,7 +58,9 @@ def generate_trials_XGB(no_trials, n_estimators_ub, n_estimators_lb,
 def generate_trials_RF(no_trials, n_estimators_ub, n_estimators_lb,
                       max_depth_ub, max_depth_lb, min_samples_leaf_ub,
                       min_samples_leaf_lb, min_samples_split_ub, min_samples_split_lb):
+    
     np.random.seed(0)
+    random.seed(0)
     hyperparameter_trials = []
     for trial in range(no_trials):
 
@@ -61,32 +68,39 @@ def generate_trials_RF(no_trials, n_estimators_ub, n_estimators_lb,
                 "n_estimators": np.random.randint(n_estimators_lb,n_estimators_ub),
                 "max_depth": np.random.randint(max_depth_lb, max_depth_ub),
                 "min_samples_leaf": np.random.randint(min_samples_leaf_lb, min_samples_leaf_ub),
-                "min_samples_split": np.random.randint(min_samples_split_lb, min_samples_split_ub)
+                "min_samples_split": np.random.randint(min_samples_split_lb, min_samples_split_ub),
+                "class_weight": random.choice([None, "balanced", {0:10, 1:1}])
                }
         hyperparameter_trials.append(trial_dict)
 
     return hyperparameter_trials
 #====================================================================================================================#
 def generate_trials_Log(no_trials, C_ub, C_lb):
+    
     np.random.seed(0)
+    random.seed(0)
     hyperparameter_trials = []
     for trial in range(no_trials):
 
         trial_dict = {
                 # logarithimic scale
-                "C": 10 ** (np.random.uniform(C_lb,C_ub)),  
+                "C": 10 ** (np.random.uniform(C_lb,C_ub)),
+                "class_weight": random.choice([None, "balanced", {0:10, 1:1}])
                }
         hyperparameter_trials.append(trial_dict)
 
     return hyperparameter_trials
 #====================================================================================================================#
 def generate_trials_KNN(no_trials, n_neighbors_ub, n_neighbors_lb):
+    
     np.random.seed(0)
+    random.seed(0)
     hyperparameter_trials = []
     for trial in range(no_trials):
 
         trial_dict = {
-                "n_neighbors": np.random.randint(n_neighbors_lb,n_neighbors_ub),  
+                "n_neighbors": np.random.randint(n_neighbors_lb,n_neighbors_ub),
+                "weights": random.choice(["uniform", "distance"])
                }
         hyperparameter_trials.append(trial_dict)
 
@@ -95,6 +109,7 @@ def generate_trials_KNN(no_trials, n_neighbors_ub, n_neighbors_lb):
 #====================================================================================================================#
 def generate_trials_ADA(no_trials, n_estimators_ub, n_estimators_lb,
                        learning_rate_lb,learning_rate_ub):
+    
     np.random.seed(0)
     hyperparameter_trials = []
     for trial in range(no_trials):
@@ -108,13 +123,17 @@ def generate_trials_ADA(no_trials, n_estimators_ub, n_estimators_lb,
     return hyperparameter_trials
 #====================================================================================================================#
 def generate_trials_SVM(no_trials, C_ub, C_lb, gamma_ub, gamma_lb):
+    
     np.random.seed(0)
+    random.seed(0)
     hyperparameter_trials = []
     for trial in range(no_trials):
 
         trial_dict = {
                 "C": 10 ** (np.random.uniform(C_lb,C_ub)), 
-            "gamma": 10 ** (np.random.uniform(gamma_lb, gamma_ub))
+            "gamma": 10 ** (np.random.uniform(gamma_lb, gamma_ub)),
+            "class_weight": random.choice([None, "balanced", {0:10, 1:1}]),
+            "kernel": random.choice(["linear", "poly", "rbf", "sigmoid"])
         }
         hyperparameter_trials.append(trial_dict)
 
